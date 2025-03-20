ee2a60f6fa1f5eda47068ac2a790a65071efb82c
Itai Hammer <itaihammer1@gmail.com>
2025-03-19 19:13:33 -0700

    Implemented small icon buttons in sidebar and created their template pages

---
+++ frontend/src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Settings () {
+    return (
+        <div style={{ textAlign: 'center', padding: '40px' }}>
+        <div className="header">
+            <h1 className="app-title">Settings</h1>
+            <p>Change settings here.</p>
+        </div>
+        </div>
+    );
+}
